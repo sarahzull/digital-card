4922e2496f5d98c91092639e9e78f6a29b7d1126
sarahzull <85895574+sarahzull@users.noreply.github.com>
2023-01-14 21:25:07 +0800

    update footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -5,34 +5,22 @@ const Footer = () => {
     <div className="Footer">
       <ul>
         <li className="twitter">
-          <a
-            href="https://twitter.com/sexy_gravy"
-            target="_blank"
-            rel="noreferrer"
-          >
+          <a href="#!">
             <i class="fa-brands fa-square-twitter"></i>
           </a>
         </li>
         <li className="facebook">
-          <a
-            href="https://www.facebook.com/sexy.gravy"
-            target="_blank"
-            rel="noreferrer"
-          >
+          <a href="#!">
             <i class="fa-brands fa-square-facebook"></i>
           </a>
         </li>
         <li className="instagram">
-          <a
-            href="https://www.instagram.com/ashtoyo/"
-            target="_blank"
-            rel="noreferrer"
-          >
+          <a href="#!">
             <i class="fa-brands fa-square-instagram"></i>
           </a>
         </li>
         <li className="github">
-          <a href="https://github.com/AshM10" target="_blank" rel="noreferrer">
+          <a href="#!">
             <i class="fa-brands fa-square-github"></i>
           </a>
         </li>
